7069d524cc34412fb0a00bfee478ed2b2e3b4626
Hamidreza-Fakharzadeh <hrfarzadeh@gmail.com>
2021-05-17 15:32:00 +0100

    create stack

--- tree.js
+++ tree.js
@@ -1,17 +1,17 @@
 class Nodes {
-      constructor() {
-            this.val = val;
-            this.left = null;
-            this.right = null;
-      }
+	constructor(val) {
+		this.val = val;
+		this.left = null;
+		this.right = null;
+	}
 }
 
-const a = new Nodes("a");
-const b = new Nodes("b");
-const c = new Nodes("c");
-const d = new Nodes("d");
-const e = new Nodes("e");
-const f = new Nodes("f");
+const a = new Nodes('a');
+const b = new Nodes('b');
+const c = new Nodes('c');
+const d = new Nodes('d');
+const e = new Nodes('e');
+const f = new Nodes('f');
 
 //      a
 //     / \
@@ -25,4 +25,18 @@ b.left = d;
 b.right = e;
 c.right = f;
 
+const deptFirstLog = (root) => {
+	const stack = [ root ];
+	while (stack.length > 0) {
+		const curr = stack.pop();
+            console.log(curr.val);
+		if (curr.left !== null) {
+			stack.push(curr.left);
+		}
+		if (curr.right !== null) {
+			stack.push(curr.right);
+		}
+	}
+};
 
+deptFirstLog(a);
